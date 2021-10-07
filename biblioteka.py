4facf017c091353d6e535c5fe14bf0f852f1b5bb
ItsaPlant <krzysiekmichalik6@gmail.com>
2021-10-07 18:05:30 +0200

    second import error commit

--- biblioteka.py
+++ biblioteka.py
@@ -1,6 +1,6 @@
 from app import app, db
 from app.models import Book, Author
-from app.db_service import book, author
+#from app.db_service import book, author
 
 @app.shell_context_processor
 def make_shell_context():
