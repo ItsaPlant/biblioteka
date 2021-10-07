69f14aa9e601458ab4ce65ffc1b4f015ade86807
ItsaPlant <krzysiekmichalik6@gmail.com>
2021-10-07 15:15:00 +0200

    second commit

--- app/models.py
+++ app/models.py
@@ -7,12 +7,47 @@ authors_to_books = db.Table('authors',
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(200))
     title = db.Column(db.String(200), index=True)
-    status = db.Column(db.String(200), index=True)
+    author = db.Column(db.String(200))
+    status = db.Column(db.String(200))
 
     def __str__(self):
-        return f"<Book {self.id} {self.title}>"
+        return f"id:{self.id}, {self.title} - {self.author}, {self.status}>"
+
+    def get_books(self):
+        data = Book.query.all()
+        dict_list=[]
+        for b in data:
+            d={
+            'id': b.id,
+            'author': b.author,
+            'title': b.title,
+            'status': b.status
+            }
+            dict_list.append(d)
+        return dict_list
+
+    def get_book(self, title):
+        data = Book.query.filter_by(title=title).first()
+        data_dict = {
+            'id': data.id,
+            'author': data.author,
+            'title': data.title,
+            'status': data.status
+        }
+        return data_dict
+
+    def post_book(self, author, title, status):
+        u = Book(author=author, title=title, status=status)
+        db.session.add(u)
+        db.session.commit()
+
+    def post_book_update(self, author, title, status):
+        data = self.get_book(title)
+        u = Book(author=author, title=title, ststus=status)
+        db.session.delete(data)
+        db.session.add(u)
+        db.session.commit()
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,5 +56,39 @@ class Author(db.Model):
 
     bibliography = db.relationship('Book', secondary=authors_to_books, lazy='subquery',
         backref=db.backref('authors', lazy=True))
+
     def __str__(self):
-        return f"<Author {self.id} {self.name} {self.surname}>"
+        return f"<Author {self.id}, {self.name}>"
+
+    def get_authors(self):
+        data = Author.query.all()
+        dict_list=[]
+        for a in data:
+            d={
+            'id': a.id,
+            'author': a.name,
+            'title': a.books
+            }
+            dict_list.append(d)
+        return dict_list
+
+    def get_author(self, name):
+        data = Author.query.filter_by(name=name).first()
+        data_dict = {
+            'id': data.id,
+            'author': data.name,
+            'title': data.books
+        }
+        return data_dict
+
+    def post_author(self, name, books):
+        u = Author(name=name, books=books)
+        db.session.add(u)
+        db.session.commit()
+
+    def post_author_update(self, name, books):
+        data = self.get_author(name)
+        u = Author(name=name, books=books)
+        db.session.delete(data)
+        db.session.add(u)
+        db.session.commit()
---
+++ app/routes.py
@@ -0,0 +1,60 @@
+from app import app, db
+from models import Book, Author
+
+app.route("/api/v1/lib/", methods=["GET"])
+def lib_list_api_v1():
+    return Book.get_books()
+
+@app.route("/api/v1/lib/", methods=["POST"])
+def add_book():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    post = {
+        'id': posts.all()[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    posts.create(post)
+    return jsonify({'post': post})
+
+@app.route("/api/v1/lib/<int:post_id>", methods=["GET"])
+def get_post(post_id):
+    post = posts.get(post_id)
+    if not post:
+        abort(404)
+    return jsonify({'post': post})
+
+@app.route("/api/v1/lib/<int:post_id>", methods=["PUT"])
+def post_update(post_id):
+    post = posts.get(post_id)
+    if not post:
+        abort(404)
+    if not request.json:
+        abort(400)
+    data = request.json
+    if any([
+        'title' in data and not isinstance(data.get('title'), str),
+        'description' in data and not isinstance(data.get('description'), str),
+        'done' in data and not isinstance(data.get('done'), bool)
+    ]):
+        abort(400)
+    post = {
+        'title': data.get('title', post['title']),
+        'description': data.get('description', post['description']),
+        'done': data.get('done', post['done'])
+    }
+    posts.update(post_id, post)
+    return jsonify({'post': post})
+###
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found', 'status_code': 404}), 404)
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request', 'status_code': 400}), 400)
+
+if __name__ == "__main__":
+    app.run(debug=True)
